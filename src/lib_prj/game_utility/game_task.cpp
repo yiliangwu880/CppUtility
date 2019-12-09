c105a27c89be21b44ca906f0db4ab2959280060e
yiliangwu880 <30390012@qq.com>
2019-12-09 16:57:20 +0800

    完善任务功能

--- src/lib_prj/game_utility/game_task.cpp
+++ src/lib_prj/game_utility/game_task.cpp
@@ -1,15 +1,129 @@
 
 #include "game_task.h"
 
+//意义和TaskTypeInfo一样，为了容易人工编写
+struct  TaskTypeStrInfo
+{
+	std::vector<std::string> vec_para_opt;
+	std::string finish_opt;
+	bool is_last_para_absolute;
+};
+
+using TaskType2Cfg = std::map<TaskType, TaskTypeStrInfo>;
+
+namespace {
+	const TaskType2Cfg & GetTaskType2Cfg()
+	{
+		static TaskType2Cfg cfg =
+		{
+			{//para1==update::para1，任务进度 >= para2 ，Update传入的最后一个参数是累加值
+				TaskType::KILL_MONSTER,
+				{{"==",}, ">=", false,}
+			},
+			{//任务进度 >= para1 ，Update传入的最后一个参数是累加值
+				TaskType::LV,
+				{{}, ">=", true,}
+			},
+			{//获取id物品， >= 品质， >= para2
+				TaskType::GET_QUALITY_ITEM,
+				{{"==", ">="}, ">=", false,}
+			},
+			{
+				TaskType::GET_ITEM,
+				{{"=="}, ">=", false,}
+			},
+		};
+		return cfg;
+	}
+}
+
+
+//任务类型信息配置
+class GameTaskTypeMgr
+{
+private:
+	std::map<TaskType, TaskTypeInfo> m_cfg;
+public:
+	static const GameTaskTypeMgr &Instance()
+	{
+		static GameTaskTypeMgr d;
+		return d;
+	}
+	const std::map<TaskType, TaskTypeInfo> &GetCfg() const { return m_cfg; }
+	bool IsCfgOk();//检查配置非法
+private:
+	TaskParaOpt Str2TaskParaLogic(const std::string &s);
+	GameTaskTypeMgr();
+};
 
 
 
+bool GameTaskTypeMgr::IsCfgOk()
+{
+	const TaskType2Cfg &str_cfg = GetTaskType2Cfg();
+	for (const auto &v : str_cfg)
+	{
+		const TaskTypeStrInfo &cfg = v.second;
+		for (const std::string &s : cfg.vec_para_opt)
+		{
+			if (TaskParaOpt::NONE == Str2TaskParaLogic(s))
+			{
+				return false;
+			}
+		}
+
+		if (TaskParaOpt::NONE == Str2TaskParaLogic(cfg.finish_opt))
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+TaskParaOpt GameTaskTypeMgr::Str2TaskParaLogic(const std::string &s)
+{
+	if (s == "==")
+	{
+		return TaskParaOpt::EQUAL;
+	}
+	else if (s == ">=")
+	{
+		return TaskParaOpt::GREATER;
+	}
+	else if (s == "<=")
+	{
+		return TaskParaOpt::LESS;
+	}
+	else
+	{
+		L_ERROR("unknow logic type %s", s.c_str());
+		return TaskParaOpt::NONE;
+	}
+}
+
+
+GameTaskTypeMgr::GameTaskTypeMgr()
+{
+	//字符串配置转换 配置，加快效率
+	const TaskType2Cfg &str_cfg = GetTaskType2Cfg();
+	for (const auto &v : str_cfg)
+	{
+		const TaskTypeStrInfo &cfg = v.second;
+		TaskTypeInfo &m_cfg_v = m_cfg[v.first];
+		for (const std::string &s : cfg.vec_para_opt)
+		{
+			m_cfg_v.vec_para_opt.push_back(Str2TaskParaLogic(s));
+		}
+		m_cfg_v.finish_opt = Str2TaskParaLogic(cfg.finish_opt);
+		m_cfg_v.is_last_para_absolute = cfg.is_last_para_absolute;
+	}
+}
 
 TaskMgr::~TaskMgr()
 {
 	for (auto &v : m_type_2_vec_task)
 	{
-		for (BaseTask_ *task : v.second)
+		for (BaseTask *task : v.second)
 		{
 			delete task;
 		}
@@ -20,13 +134,18 @@ TaskMgr::~TaskMgr()
 
 bool TaskMgr::UnRegTask(const TaskCfg &cfg)
 {
+	if (!m_is_enable_unreg)
+	{
+		L_ERROR("error call UnRegTask")
+		return false;
+	}
 	auto it = m_type_2_vec_task.find((TaskType)cfg.task_type);
 	if (it == m_type_2_vec_task.end())
 	{
 		return false;
 	}
 	VecTask &vec = it->second;
-	for (BaseTask_ *task : vec)
+	for (BaseTask *task : vec)
 	{
 		if (cfg.id == task->GetCfg().id)
 		{
@@ -58,32 +177,32 @@ uint32 TaskMgr::GetCfgPara(const TaskCfg &cfg, uint32 idx) const
 		L_ERROR("idx too big. %d", idx); //target_type_info.vec_para_logic 定义的size过大
 		return 0;
 		break;
-	case 0: return cfg.para1; break;
-	case 1: return cfg.para2; break;
-	case 2: return cfg.para3; break;
-	case 3: return cfg.para4; break;
-	case 4: return cfg.para5; break;
+	case 0: return cfg.para0; break;
+	case 1: return cfg.para1; break;
+	case 2: return cfg.para2; break;
+	case 3: return cfg.para3; break;
+	case 4: return cfg.para4; break;
 	}
 }
 
 
-bool TaskMgr::IsLogicOk(TaskParaLogic logic, int64 cfg_last_para, int64 cur_num) const
+bool TaskMgr::IsLogicOk(TaskParaOpt logic, int64 cfg_last_para, int64 cur_num) const
 {
-	if (logic == TaskParaLogic::GREATER)
+	if (logic == TaskParaOpt::GREATER)
 	{
 		if (cur_num >= cfg_last_para)
 		{
 			return true;
 		}
 	}
-	else if (logic == TaskParaLogic::LESS)
+	else if (logic == TaskParaOpt::LESS)
 	{
 		if (cur_num <= cfg_last_para)
 		{
 			return true;
 		}
 	}
-	else if (logic == TaskParaLogic::EQUAL)
+	else if (logic == TaskParaOpt::EQUAL)
 	{
 		if (cur_num == cfg_last_para)
 		{
@@ -101,6 +220,7 @@ bool TaskMgr::IsLogicOk(TaskParaLogic logic, int64 cfg_last_para, int64 cur_num)
 
 void TaskMgr::Update(TaskType task_type, ...)
 {
+	m_is_enable_unreg = false;
 	va_list args;
 	va_list forward_args; //转传给IsMatchCondition函数用
 	va_start(args, task_type);
@@ -109,20 +229,21 @@ void TaskMgr::Update(TaskType task_type, ...)
 	auto it = m_type_2_vec_task.find(task_type);
 	if (it == m_type_2_vec_task.end())
 	{
+		m_is_enable_unreg = true;
 		return;
 	}
 	VecTask &vec_task = it->second;
 
-	const auto it2 = GameTaskTypeMgr::Instance().GetCfg().find(task_type);
-	if (it2 == GameTaskTypeMgr::Instance().GetCfg().end())
+	const TaskTypeInfo *type_detail = wyl::MapFind(GameTaskTypeMgr::Instance().GetCfg(), task_type);
+	if (nullptr == type_detail)
 	{
 		L_ERROR("unknow task type %d", (int)task_type);
+		m_is_enable_unreg = true;
 		return;
 	}
-	const GameTaskTypeDetail &type_detail = it2->second;
 
 	std::vector<typename VecTask::value_type> remove_task; //完成等删除的任务
-	uint32 para_num = type_detail.vec_para_logic.size();
+	uint32 para_num = type_detail->vec_para_opt.size();
 	if (para_num>0)
 	{
 		for (int32 i = 0; i < (int32)para_num; ++i)
@@ -133,18 +254,18 @@ void TaskMgr::Update(TaskType task_type, ...)
 	const uint32 last_para = va_arg(args, uint32);
 
 	//task_state 和 type_detail 比较，不符合条件退出, 符合改变进度
-	for (BaseTask_ *base_task : vec_task)
+	for (BaseTask *base_task : vec_task)
 	{
 		const TaskCfg &task_cfg = base_task->GetCfg();
 
 		//判断是否符合条件
-		if (!IsMatchCondition(type_detail, task_cfg, forward_args))
+		if (!IsMatchCondition(*type_detail, task_cfg, forward_args))
 		{
 			break;
 		}
 
 		//符合条件，判断最后一个参数
-		if (type_detail.is_last_para_absolute)//is_last_para_absolute true表示最后一个参数表示绝对值，false 表示累加值. default 表示累加
+		if (type_detail->is_last_para_absolute)//is_last_para_absolute true表示最后一个参数表示绝对值，false 表示累加值. default 表示累加
 		{
 			base_task->SetNum(last_para);
 		}
@@ -154,7 +275,7 @@ void TaskMgr::Update(TaskType task_type, ...)
 		}
 
 		int64 cfg_last_para = GetCfgPara(task_cfg, para_num);
-		TaskParaLogic logic = type_detail.finish_logic;
+		TaskParaOpt logic = type_detail->finish_opt;
 		if (IsLogicOk(logic, cfg_last_para, base_task->GetNum()))
 		{
 			base_task->OnFinish();
@@ -171,15 +292,16 @@ void TaskMgr::Update(TaskType task_type, ...)
 		delete v;
 		wyl::VecRemove(vec_task, v);
 	}
+	m_is_enable_unreg = true;
 }
 
 
-bool TaskMgr::IsMatchCondition(const GameTaskTypeDetail &type_detail, const TaskCfg &task_cfg, va_list args) const
+bool TaskMgr::IsMatchCondition(const TaskTypeInfo &type_detail, const TaskCfg &task_cfg, va_list args) const
 {
 	int idx = 0;
-	int last_idx = type_detail.vec_para_logic.size() - 1;
+	int last_idx = type_detail.vec_para_opt.size() - 1;
 	uint32 cfg_para = 0;
-	for (const auto &logic : type_detail.vec_para_logic)
+	for (const auto &logic : type_detail.vec_para_opt)
 	{
 		cfg_para = GetCfgPara(task_cfg, idx);
 		uint32 para = va_arg(args, uint32);
@@ -191,3 +313,5 @@ bool TaskMgr::IsMatchCondition(const GameTaskTypeDetail &type_detail, const Task
 	}
 	return true;
 }
+
+
--- src/lib_prj/game_utility/game_task.h
+++ src/lib_prj/game_utility/game_task.h
@@ -3,40 +3,50 @@
 author:YiliangWu
 brief:
 任务功能：
-	通用地实现了往玩家身上加入任务，任务进度变化，结束功能。 不能直接复用，需要根据用户项目修改下配置字段。
+	通用地实现了往玩家身上加入任务，任务进度变化，结束功能。 不能直接复用，需要修改配置字段。
 
-待优化：
-GameTaskTypeDetail配置 改为放到策划配置
-反正都不不修改就复用，就不用模板，简化问题。直接定义TaskCfg，用户根据具体项目再修改 TaskCfg。 
+使用说明：
+	可能需要根据项目修改下配置TaskCfg的字段名。
+	配置：
+	{
+		新任务类型， 需要TaskType加类型， GetTaskType2Cfg函数里面加类型操作信息.
+		新任务， 策划配置TaskCfg。
+	}
+	代码：
+	{
+		看需要定义新任务类：
+		{
+			比如class DeriveTask: public BaseTask
+			实现任务进度变化回调函数。
+			实现任务进度完成回调函数。
+		}
+
+		任务系统注册任务调用： TaskMgr::RegTask<DeriveTask>
+		任务更新触发调用：TaskMgr::Update(task_type, ...)
+	}
+
+设置思想：
+	本来配置信息可以做出模板，但代码会更复杂。这里选修改代码的方式使用，一个项目通常就一次修改，就能吧模板问题简化掉。
 
 
-新加任务需要做:
-新任务类型，需要GameTaskTypeMgr构造函数里面加GameTaskTypeDetail配置. 
-新任务， 策划配置按照 GameTaskTypeDetail 要求填值
-
-看需要定义新任务类：比如class DeriveTask: public BaseTask
-注册任务事件调用： TaskMgr<TaskCfg>::RegTask
-任务更新触发调用：[TaskMgr<TaskCfgTaskCfg>::Update(task_type, ...)]
-
-使用例子：
-	class DeriveTask: public BaseTask<TaskCfg>
+代码参考：
+	class GetItemTask: public BaseTask
 	{
 	public:
-		DeriveTask(const MyTaskCfg &cfg 这个参数必须放第一 , Player &player, int64 num = 0);
+		GetItemTask(const TaskCfg &cfg 这个参数必须放第一 , Player &player, int64 num = 0);
 		virtual void OnFinish() override{};//可以做存档操作，比如存task_id, num, is_finish
 		virtual void OnUpdate()override{};//可以做存档操作，比如存task_id, num
 	}
 
 	//注册任务
-	TaskMgr<TaskCfg> mgr;
+	TaskMgr mgr;
 	Player player;
 	TaskCfg cfg; 
 	...
-	mgr.RegTask<DeriveTask>(cfg, player, [1]);
+	mgr.RegTask<GetItemTask>(cfg, player, [1]);
 	...
-
 	//当触发任务改变事件发生时调用
-	obj:Update(target_type.GET_ITEM, 1002, 1)；
+	mgr:Update(TaskType::GET_ITEM, 1002, 1)；
 
 */
 /************************************************************************/
@@ -46,22 +56,34 @@ GameTaskTypeDetail配置 改为放到策划配置
 #include "../utility/typedef.h"
 #include <map>
 #include <algorithm>
-#include "game_task_cfg.h"
 #include <stdarg.h>
 #include "../utility/stlBoost.h"
 
 
+enum class TaskType
+{
+	KILL_MONSTER = 1,
+	LV = 2,
+	GET_QUALITY_ITEM = 3,
+	GET_ITEM = 4,
+};
+
 //策划配置模板，根据不同项目改命名
 struct  TaskCfg
 {
-	uint32 id;
-	uint32 task_type;		//GameTaskType
-	uint32 para1, para2, para3, para4, para5; //选用参数
+	uint32 id;	                              //任务id,唯一就可以了
+	uint32 task_type;		                  //GameTaskType
+	uint32 para0, para1, para2, para3, para4; //选用参数
 };
 
+//任务基类
 //派生类的构造函数，第一个参数必须为 const TaskCfg &
 class BaseTask
 {
+private:
+	int64 m_num;		  //表示进度
+	const TaskCfg &m_cfg; //可以优化成读取静态配置，减少内存
+
 public:
 	//para num 任务初始化进度
 	BaseTask(const TaskCfg &cfg, int64 num)
@@ -79,17 +101,32 @@ public:
 	void SetNum(int64 num){ m_num = num; }
 	void AddNum(int64 add){ m_num += add; }
 	const int64 GetNum(){ return m_num; }
-protected:
-	int64 m_num; //表示进度
-	const TaskCfg &m_cfg;
 };
 
+//任务参数逻辑
+enum class TaskParaOpt
+{
+	NONE,
+	EQUAL = 0,      //TaskMgr::Update的参数 == 配置参数
+	GREATER = 1,    //TaskMgr::Update的参数 >= 配置参数
+	LESS = 2,       //TaskMgr::Update的参数 <= 配置参数
+};
 
+//配置所有任务类型
+struct TaskTypeInfo
+{
+	//表达 TaskMgr::Update参数和任务配置参数的关系。 
+	//vec_para_logic[0] 表示update参数1 para1的关系，  vec_para_logic[1]表示。。。。
+	std::vector<TaskParaOpt> vec_para_opt;
+	// task_state.num(任务进度) 和 TaskCfg.paraX 最后一个有效参数（达成目标）的关系。
+	TaskParaOpt finish_opt;
+	// true表示从Update传入的最后一个参数是绝对值，否则表示累加值.
+	bool is_last_para_absolute;
+};
 
 class TaskMgr
 {
 public:
-	using BaseTask_ = BaseTask;
 	~TaskMgr();
 	template<class DeriveTask, class... Args>
 	bool RegTask(const TaskCfg &cfg, Args&&... args);
@@ -103,18 +140,19 @@ public:
 private:
 	uint32 GetCfgPara(const TaskCfg &cfg, uint32 idx) const;
 	//true表示 cur_num和cfg_last_para 符合逻辑关系 logic
-	bool IsLogicOk(TaskParaLogic logic, int64 cfg_last_para, int64 cur_num) const;
+	bool IsLogicOk(TaskParaOpt logic, int64 cfg_last_para, int64 cur_num) const;
 	//判断非最后一个参数决定的任务条件是否符合
 	//	para target_type_info 为 target_type_infos的一个元素
 	//	para ..., Update传进来的可变参数。 格式为 para1, para2, para3 
 	//return true表示符合条件
-	bool IsMatchCondition(const GameTaskTypeDetail &target_type_info, const TaskCfg &task_cfg, va_list args) const;
+	bool IsMatchCondition(const TaskTypeInfo &target_type_info, const TaskCfg &task_cfg, va_list args) const;
 
 private:
-	using VecTask = std::vector<BaseTask_ *>;
+	using VecTask = std::vector<BaseTask *>;
 	using Type2VecTask = std::map<TaskType, VecTask>;
 
 	Type2VecTask m_type_2_vec_task;
+	bool m_is_enable_unreg = true; //放错误调用UnRegTask
 };
 
 
@@ -130,7 +168,7 @@ template<class DeriveTask, class... Args>
 bool TaskMgr::RegTask(const TaskCfg &cfg, Args&&... args)
 {
 	VecTask &vec = m_type_2_vec_task[(TaskType)cfg.task_type];
-	for (BaseTask_ *task : vec)
+	for (BaseTask *task : vec)
 	{
 		if (cfg.id == task->GetCfg().id)
 		{
