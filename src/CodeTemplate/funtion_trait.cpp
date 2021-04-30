c59a3e6359987c324ff817eef9491068556073cd
yiliangwu880 <30390012@qq.com>
2021-04-30 15:53:55 +0800

    记录 std::function 获取函数参数类型的方法

--- src/CodeTemplate/funtion_trait.cpp
+++ src/CodeTemplate/funtion_trait.cpp
@@ -1,3 +1,44 @@
+方法1：利用std::function
+//通过函数名，获取函数参数，返回值 类型 。
+//只能适用 1-2个参数的函数。
+namespace
+{
+	void f1(int a, uint64_t b)
+	{
+
+	}
+	void f2(uint8_t a, uint64_t b)
+	{
+
+	}
+	int f3(uint8_t a)
+	{
+		return 0;
+	}
+	template<class Fun>
+	void RegProtoParse3(Fun f)
+	{
+		//typename std::function<Fun>::argument_type t2 = 231; //ok
+		typename std::function<Fun>::first_argument_type t2 = 231; //ok
+		typename std::function<Fun>::second_argument_type t1 = 231; //ok
+		typename std::function<Fun>::result_type *p = nullptr;
+
+		UNIT_INFO("t2 %d %d", t2, sizeof(t2))
+			UNIT_INFO("para3");
+	}
+}
+
+UNITTEST(fun3)
+{
+	//RegProtoParse3<decltype(f2)>(f2);
+	RegProtoParse3<decltype(f1)>(f1);
+	//RegProtoParse3<decltype(f3)>(f3);
+}
+
+
+
+方法2: 研究发现不能像方法1那样抽象用，待研究。
+///////////////////////////////////////////////////
 function_traits通过模板特化和可变参数模板来获取函数类型和返回类型
 以下源内容来自于《深入应用C++11 代码优化与工程级应用》3.3.6　function_traits
 
